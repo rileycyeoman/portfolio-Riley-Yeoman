2efb72720bc8e83f61cea7583edec1fd2f837c7d
Riley Yeoman <92952728+rileycyeoman@users.noreply.github.com>
2024-10-04 00:32:43 -0500

    added flying donut????

--- script.js
+++ script.js
@@ -32,11 +32,52 @@ loader.load('./models/robot_arm/scene.gltf', function(gltf) {
 });
 
 
+
+
+// Solid material
+var geometry = new THREE.IcosahedronGeometry(1); // Radius of 1
+var solidMaterial = new THREE.MeshPhongMaterial({ color: 0x00FFFF, flatShading: true }); // Solid green material
+var icosahedronSolid = new THREE.Mesh(geometry, solidMaterial);
+scene.add(icosahedronSolid);
+
+
+
+// Wireframe material
+var wireframeMaterial = new THREE.MeshBasicMaterial({ color: 0x000000, wireframe: true }); // Wireframe (white)
+var icosahedronWireframe = new THREE.Mesh(geometry, wireframeMaterial);
+scene.add(icosahedronWireframe);
+
+
+icosahedronSolid.position.set(-6, -1, 0);
+icosahedronWireframe.position.set(-6, -1, 0);
+
+
+var torusGeometry = new THREE.TorusGeometry(1, 0.3, 16, 100); // Large torus
+var torusMaterial = new THREE.MeshPhongMaterial({ color: 0xff6347 }); // Red torus
+var torus = new THREE.Mesh(torusGeometry, torusMaterial);
+scene.add(torus);
+torus.position.set(-6, 1, 0); // Position it to the left of the icosahedron
+
+
+var time = 0;
 camera.position.set(0,1,5);
 
 // Render loop
 var animate = function() {
     requestAnimationFrame(animate);
+
+    // Rotate both the solid and wireframe icosahedron
+    icosahedronSolid.rotation.x += 0.001;
+    icosahedronSolid.rotation.y += 0.001;
+
+    icosahedronWireframe.rotation.x += 0.001;
+    icosahedronWireframe.rotation.y += 0.001;
+
+    time += 0.05;
+    torus.position.x = Math.sin(time) * 2;  // Move along x-axis
+    torus.position.y = Math.sin(time * 0.5) * 1;  // Wave up and down
+    torus.rotation.z += 0.05;  // Rotate the torus around its z-axis
+
 
     // base.rotation.y += 0.01;
     renderer.render(scene, camera);
