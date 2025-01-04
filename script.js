4552a4d842cbcac5d8b538b8a21c322fb4fc6716
Riley Yeoman <92952728+rileycyeoman@users.noreply.github.com>
2025-01-04 16:33:00 -0600

    for resume reasons

--- script.js
+++ script.js
@@ -100,14 +100,12 @@ thing.mesh.scale.set(0.2,0.2,0.2);
 
 const texts = [
     "Now with more Javascript!",
-    "A Wolf in Developer's Clothing!",
     "Based in Minnesota!",
-    "Robots? They make those now?!",
+    "Robots?!",
     "90% Bug Free!",
     "I'm Always A Call (or 2) Away!",
     "Now in 2D!",
-    "No, It's A Hootenanny!",
-    "Who You Calling A Pinhead?",
+    "It's A Hootenanny!",
   ];
   
   // Select a random text
